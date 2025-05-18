c6c1cdef5482911c637fb1dbfd76f7ea307cce11
Brian Maleek <maleekmalik@gmail.com>
2025-05-18 03:54:41 +0300

    Refactor SQL queries for user bookings and property rankings by removing redundant ORDER BY clause and improving readability

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,24 +12,23 @@ GROUP BY
     u.user_id,
     u.first_name,
     u.last_name
-ORDER BY
-    total_bookings DESC;
+
+-- Property rankings based on booking count using window functions
 
 -- 2. Use a window function (ROW_NUMBER, RANK) to rank properties based on the total number of bookings they have received.
-SELECT
+
+SELECT 
     p.property_id,
     p.name AS property_name,
     p.location,
-    COUNT(b.booking_id) AS booking_count,
-    ROW_NUMBER () OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank,
-    RANK () OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank_with_ties
-FROM
-    properties AS p
-LEFT JOIN
-    bookings AS b ON p.property_id = b.property_id
-GROUP BY
-    p.property_id,
-    p.name,
-    p.location
-ORDER BY
+    COUNT(b.booking_id) as booking_count,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) as booking_rank,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) as booking_rank_with_ties
+FROM 
+    properties p
+LEFT JOIN 
+    bookings b ON p.property_id = b.property_id
+GROUP BY 
+    p.property_id, p.name, p.location
+ORDER BY 
     booking_count DESC;
